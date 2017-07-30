e2106c0bef9accec7d5fdd89a78ffeafbf1cb172
Jabal Torres <info@jabaltorres.com>
2017-07-30 14:22:40 -0700

    fonts and color added

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Digital Times Transformation</title>
     <meta name="description" content="The leading souce for cyber security professionals.">
-    <link href="https://fonts.googleapis.com/css?family=Oswald|Roboto" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+
     <link rel="stylesheet" href="css/app.css">
   </head>
   <body>
@@ -19,7 +21,9 @@
     <div class="header">
       <div class="row">
         <div class="medium-7 columns">
-          <a class="logo" href="#">Digital Times Transformation</a>
+          <a class="" href="#">
+            <img class="logo" src="images/dtt-logo.svg" alt="Digital Times Transformation">
+          </a>
         </div>
         <div class="medium-5 columns main-navigation">
           <ul>
@@ -32,17 +36,23 @@
 
     <div class="hero">
       <div class="row">
-        <div class="large-12 columns">
-          <h1>The cost of a bad executive hire could cost the company $1.5 million</h1>
+        <div>
+          <div class="large-12 columns">
+            <div class="carousel">
+              <h1>A bad executive hire could cost your company&nbsp;millions</h1>
+              <h1>By 2019 1.5 million Cyber Professional jobs will be&nbsp;unfilled</h1>
+              <h1>Most cyber attacks go undetected for 230&nbsp;days</h1>
+            </div>
+          </div>
+          <div class="large-9 large-centered columns">
+            <span class="separator"></span>
+            <p>Cyber Security is the most complex issue facing Business Leaders today. It's not about IT, it's about Risk Management. Partnering with the right expert is essential in protecting your&nbsp;organization. <span class="dtt">Digital Times Transformation solves those issues.</span></p>
+          </div>
+          <a class="button large" href="#contact">Contact Us</a>
         </div>
-        <div class="large-9 large-centered columns">
-          <span class="separator"></span>
-          <p>Cyber Security is the most complex issue facing Business Leaders today. It's not about IT, it's about Risk Management. Partnering with the right expert is essential in protecting your&nbsp;organization. <span class="dtt">Digital Times Transformation solves those issues.</span></p>
-        </div>
-        <a class="button large" href="#contact">Contact Us</a>
-      </div>        
+      </div>  
     </div>
-
+    
     <div id="about-us" class="value">
       <div class="row">
         <div class="large-12 columns">
@@ -50,17 +60,17 @@
           <span class="separator"></span>
         </div>
         <div class="large-4 columns">
-          <img src="images/advisory-board.svg">
+          <img class="icon" src="images/advisory-board.svg">
           <h4>Global Advisory Board</h4>
           <p>Get unparalleled access to our world class resources through our globally recognized cyber security advisory board. Talk to us about our cyber-on-demand services exclusively available to you as our client.</p>
         </div>
         <div class="large-4 columns">
-          <img src="images/executive-recruiting.svg">
+          <img class="icon" src="images/executive-recruiting.svg">
           <h4>Executive Search & Consulting</h4>
           <p>Executives impact the cultural direction of your business. A misfit hire here could damage your reputation, impact your momentum and reduce your credibility with employees and external&nbsp;stakeholders.</p>
         </div>
         <div class="large-4 columns">
-          <img src="images/cyber-security-professionals.svg">
+          <img class="icon" src="images/cyber-security-professionals.svg">
           <h4>Cyber Security Professionals</h4>
           <p>Finding the elusive cyber security professional is a niche, highly specialized process. It requires a unique skill set and a deep understanding of today's cyber landscape.</p>
         </div>
@@ -72,7 +82,8 @@
       <span class="separator"></span>
       <div class="row">
         <div class="large-6 large-centered columns">
-          <form id="my-form" action="https://formspree.io/info@jabaltorres.com" method="POST" data-abide>
+          <h4 class="locations">US | UK | Singapore</h4>
+          <form id="my-form" action="https://formspree.io/awellesley@dttransformation.com" method="POST" data-abide>
             <input type="text" name="name" placeholder="Full Name" required>
             <input type="email" name="_replyto" placeholder="username@address.com" required>
             <textarea name="message" rows="4" cols="50" placeholder="Enter your message here" required></textarea>
@@ -83,6 +94,7 @@
       <span class="separator"></span>
       <p><a class="phone-num" href="tel:1-203-391-4418">1-203-391-4418</a></p>
     </div>
+
     <div class="footer">
       <div class="row">
         <div class="large-9 columns">
@@ -93,13 +105,10 @@
         </div>
       </div>  
     </div>
-      
-    <!-- <script src="bower_components/jquery/dist/jquery.js"></script> -->
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
- 
 
-    <!-- <script src="bower_components/what-input/dist/what-input.js"></script> -->
-    <!-- <script src="bower_components/foundation-sites/dist/js/foundation.js"></script> -->
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="slick/slick.min.js"></script>
+
     <script src="js/min/app-min.js"></script>
   </body>
 </html>
--- js/app.js
+++ js/app.js
@@ -1,20 +1,31 @@
 // $(document).foundation();
+$(document).ready(function(){
 
-var dttDuration = '500';
-var dttEasing = 'swing';
+  var dttDuration = '500';
+  var dttEasing = 'swing';
 
-// scroll to target
-$('.main-navigation a, .hero a').on('click', function(event){
-  event.preventDefault();
+  // scroll to target
+  $('.main-navigation a, .hero a').on('click', function(event){
+    event.preventDefault();
 
-  $('html, body').animate({
-    scrollTop: $( $.attr(this, 'href') ).offset().top
-  }, dttDuration, dttEasing);
-});
+    $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, dttDuration, dttEasing);
+  });
 
-// scroll to top
-var scroll_top_icon = $('#scroll-to-top');
+  // scroll to top
+  var scroll_top_icon = $('#scroll-to-top');
 
-scroll_top_icon.click(function(){
-  $("html, body").animate({scrollTop:0}, dttDuration, dttEasing);
+  scroll_top_icon.click(function(){
+    $("html, body").animate({scrollTop:0}, dttDuration, dttEasing);
+  });
+  
+  // carousel
+  $('.carousel').slick({
+    autoplay:true,
+    infinite: true,
+    arrows:false,
+    speed: 1000,
+    pauseOnHover: false
+  });
 });
